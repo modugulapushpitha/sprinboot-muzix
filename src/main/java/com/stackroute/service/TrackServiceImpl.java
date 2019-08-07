ee210de32cffd926a47f49548e8a955eebe9ac97
modugulapushpitha <51950141+modugulapushpitha@users.noreply.github.com>
2019-08-07 17:35:26 +0530

    Update TrackServiceImpl.java

--- src/main/java/com/stackroute/service/TrackServiceImpl.java
+++ src/main/java/com/stackroute/service/TrackServiceImpl.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 
 @Service
 public class TrackServiceImpl implements TrackService{
-    TrackRepository trackRepository;
+    private TrackRepository trackRepository;
     @Autowired
     public TrackServiceImpl(TrackRepository trackRepository){
     this.trackRepository=trackRepository;
