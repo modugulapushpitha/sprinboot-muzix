e2d2751a0a4a6ae32210546e26f1eddd150c2155
modugulapushpitha <pushpithamodugula@gmail.com>
2019-08-08 17:53:17 +0530

    added @lombok

--- src/main/java/com/stackroute/domain/Track.java
+++ src/main/java/com/stackroute/domain/Track.java
@@ -1,5 +1,8 @@
 package com.stackroute.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 
@@ -7,61 +10,16 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
-@Entity
-@Table(name="Track")
-@PropertySource("application.properties")
-public class Track {
-    @Id
-    @Column(name="id")
-    @Value("id")
-    private int id;
-    @Column(name="name")
-    @Value("${value.name}")
-    private String name;
-    @Column(name="comment")
-    @Value("${value.comment}")
-    private String comment;
-
-    public Track() {
-    }
-
-    public Track(int id, String name, String comment) {
-        this.id = id;
-        this.name = name;
-        this.comment = comment;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
-
-    @Override
-    public String toString() {
-        return "Track{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", comment='" + comment + '\'' +
-                '}';
-    }
+    @Entity
+    @Table(name="Track")
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public class Track {
+        @Id
+        private int id;
+        @Column(name="name")
+        private String name;
+        @Column(name ="comment")
+        private String comment;
 }
